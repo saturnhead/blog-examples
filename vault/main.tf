bd7175a549dba7d7465870efa187852ffb492f3c
Flavius <flaviuscdinu@gmail.com>
2023-07-06 15:37:15 +0300

    Added vault example

---
+++ vault/main.tf
@@ -0,0 +1,28 @@
+provider "vault" {
+  address = var.vault_address
+  auth_login_jwt {
+    role = "infra-preprod"
+  }
+}
+
+data "vault_generic_secret" "this" {
+  path = "secrets/vpc"
+}
+
+locals {
+  vpc_cidr    = data.vault_generic_secret.this.data["cidr_block"]
+  subnet_cidr = cidrsubnet(local.vpc_cidr, 8, 1)
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_vpc" "this" {
+  cidr_block = local.vpc_cidr
+}
+
+resource "aws_subnet" "this" {
+  cidr_block = local.subnet_cidr
+  vpc_id     = aws_vpc.this.id
+}
---
+++ vault/variables.tf
@@ -0,0 +1,4 @@
+variable "vault_address" {
+  type    = string
+  default = "https://vault-cluster-public-vault-6379fdf7.631db3ac.z1.hashicorp.cloud:8200/"
+}
