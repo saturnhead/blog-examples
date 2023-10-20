ac92afa8174f4aff74b3fe7f684c5b4f05b7b0a9
Flavius <flaviuscdinu93@gmail.com>
2023-10-20 10:55:53 +0300

    Initial version of terraform and ansible stack dependencies

---
+++ tf-ansible-stack-dependencies/tf/main.tf
@@ -0,0 +1,46 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu*"]
+  }
+}
+
+locals {
+  instances = {
+    instance1 = {
+      ami           = data.aws_ami.ubuntu.id
+      instance_type = "t2.micro"
+    }
+    instance2 = {
+      ami           = data.aws_ami.ubuntu.id
+      instance_type = "t2.micro"
+    }
+    instance3 = {
+      ami           = data.aws_ami.ubuntu.id
+      instance_type = "t2.micro"
+    }
+  }
+}
+
+resource "aws_key_pair" "ssh_key" {
+  key_name   = "ec2"
+  public_key = file(var.public_key)
+}
+
+resource "aws_instance" "this" {
+  for_each                    = local.instances
+  ami                         = each.value.ami
+  instance_type               = each.value.instance_type
+  key_name                    = aws_key_pair.ssh_key.key_name
+  associate_public_ip_address = true
+
+  tags = {
+    Name = each.key
+  }
+}
---
+++ tf-ansible-stack-dependencies/tf/variables.tf
@@ -0,0 +1,4 @@
+variable "public_key" {
+  type    = string
+  default = "~/.ssh/id_rsa.pub"
+}
