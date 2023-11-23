839044ba2734597d2eeb02be5945bf6d68aed858
Flavius <flaviuscdinu93@gmail.com>
2023-11-23 10:49:03 +0200

    Another test for project globs

---
+++ project_globs/main.tf
@@ -0,0 +1,34 @@
+resource "null_resource" "name" {
+  provisioner "local-exec" {
+    command = "echo project globs"
+  } 
+}
+
+resource "null_resource" "name2" {
+  provisioner "local-exec" {
+    command = "echo project globs"
+  } 
+}
+
+resource "null_resource" "name3" {
+  provisioner "local-exec" {
+    command = "echo project globs"
+  } 
+}
+
+resource "null_resource" "name4" {
+  provisioner "local-exec" {
+    command = "echo project globs"
+  } 
+}
+
+resource "null_resource" "name5" {
+  provisioner "local-exec" {
+    command = "echo project globs"
+  } 
+}
+resource "null_resource" "name6" {
+  provisioner "local-exec" {
+    command = "echo project globs"
+  } 
+}
