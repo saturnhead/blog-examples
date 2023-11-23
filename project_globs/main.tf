c648d7780cbaf00b3055a2ab62ddc52c5e0cd942
Flavius <flaviuscdinu93@gmail.com>
2023-11-23 10:39:40 +0200

    Added initial version of the project_globs example

--- project_globs/main.tf
+++ project_globs/main.tf
@@ -4,7 +4,13 @@ resource "null_resource" "name" {
   } 
 }
 
-resource "null_resource" "name" {
+resource "null_resource" "name2" {
+  provisioner "local-exec" {
+    command = "echo project globs"
+  } 
+}
+
+resource "null_resource" "name3" {
   provisioner "local-exec" {
     command = "echo project globs"
   } 
