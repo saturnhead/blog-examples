5479eb2e2e348f522d2fd24d390fbd34df215f12
Flavius <flaviuscdinu93@gmail.com>
2023-12-12 14:05:50 +0200

    Added another instance in the tf ansible stack dependency example

--- tf-ansible-stack-dependencies/tf/main.tf
+++ tf-ansible-stack-dependencies/tf/main.tf
@@ -36,6 +36,10 @@ locals {
       ami           = data.aws_ami.ubuntu.id
       instance_type = "t2.micro"
     }
+    instance4 = {
+      ami           = data.aws_ami.ubuntu.id
+      instance_type = "t2.micro"
+    }
   }
 }
 
