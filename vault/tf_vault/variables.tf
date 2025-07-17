66523dd3c229260b9497a43b5da2624ce1a326df
Flavius Dinu <57911064+flavius-dinu@users.noreply.github.com>
2025-07-17 10:35:12 +0300

    Update variables.tf

--- vault/tf_vault/variables.tf
+++ vault/tf_vault/variables.tf
@@ -1,4 +1,4 @@
 variable "vault_address" {
   type    = string
-  default = "https://vault-cluster-public-vault-6379fdf7.631db3ac.z1.hashicorp.cloud:8200/"
+  default = "https://vault-cluster-public-vault-0902e0c9.7577bb7a.z1.hashicorp.cloud:8200"
 }
